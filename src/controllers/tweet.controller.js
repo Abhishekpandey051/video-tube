6d9ef4aa47111ffb43db37fd26fbb198dee08f1e
Abhishek Pandey <abhi.ecc.cs@gmail.com>
2025-09-08 23:34:35 +0530

    delete and update tweet - API

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -2,6 +2,7 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { Tweet } from "../model/tweet.model.js";
+import { isValidObjectId } from "mongoose";
 
 // post tweet on video - API
 const addTweet = asyncHandler(async (req, res) => {
@@ -25,4 +26,42 @@ const addTweet = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "Tweet added successfully"));
 });
 
-export { addTweet };
+// update tweet - API
+const updateTweet = asyncHandler(async (req, res) => {
+    const {tweetId} = req.params;
+    const {tweet} = req.body;
+    if(!tweetId){
+        throw new ApiError(400, "Tweet id is required")
+    }
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "Invalid tweet id")
+    }
+    const gettweet = await Tweet.findById(tweetId)
+    if(!gettweet){
+        throw new ApiError(404, "Tweet not found")
+    }
+    if(gettweet.owner.toString() !== req.user?._id.toString()){
+        throw new ApiError(403, "You are not authorized to update this tweet")
+    }
+    gettweet.content = tweet
+    const updatedTweet = await gettweet.save({validateBeforeSave: false})
+    if(!updatedTweet){
+        throw new ApiError(500, "Something went wrong while updating tweet")
+    }
+    return res.status(200).json(new ApiResponse(200, {}, "Tweet updated successfully"))
+})
+
+// Delete tweet - API
+const deleteTweet = asyncHandler(async(req, res) => {
+    const {tweetId} = req.params;
+    if(!isValidObjectId(tweetId)){
+        throw new ApiError(400, "Invalid tweet id")
+    }
+    const deleteTweet = await Tweet.findByIdAndDelete(tweetId)
+    if(!deleteTweet){
+        throw new ApiError(404, "Tweet not found")
+    }
+    return res.status(200).json(new ApiResponse(200, {}, "Tweet deleted successfully"))
+})
+
+export { addTweet, updateTweet, deleteTweet };
--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { verifyJwt } from "../middleware/auth.middleware.js";
-import { addTweet } from "../controllers/tweet.controller.js";
+import { addTweet, deleteTweet, updateTweet } from "../controllers/tweet.controller.js";
 
 const router = Router();
 
 router.route("/").post(verifyJwt, addTweet);
+router.route("/update/:tweetId").patch(verifyJwt, updateTweet)
+router.route("/delete/:tweetId").delete(verifyJwt, deleteTweet)
 
 export default router;
